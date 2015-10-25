c9a709756c6ba2fe544fe0c460f30d0f58259dac
husnasidd <hsiddiqu@citadel.edu>
2015-10-25 20:33:04 +0000

    done

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -33,6 +33,13 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   end
 end
 
+
+
+# And /I press "([^\"]*)"$/ do |button|
+#   click_button(button)
+# end
+
+
 Then /I (should|should not) see the following movies: (.*)/ do |should_or_not, movie_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
